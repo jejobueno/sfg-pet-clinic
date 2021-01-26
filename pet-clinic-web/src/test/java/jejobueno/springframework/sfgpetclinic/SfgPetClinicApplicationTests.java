36979d7d4ea2e3e0f375cd349d410435f41cc715
Jesus Bueno <jessbuenos@gmail.com>
2021-01-26 17:01:59 +0100

    error in package name fixed

--- pet-clinic-web/src/test/java/jejobueno/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/jejobueno/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,4 +1,4 @@
-package guru.springfralework.sfgpetclinic;
+package jejobueno.springfralework.sfgpetclinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
