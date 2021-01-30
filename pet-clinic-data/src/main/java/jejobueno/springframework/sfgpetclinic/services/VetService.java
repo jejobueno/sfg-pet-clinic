342895513854cf10d674775ea9da653f2c29a200
Jesus Bueno <jessbuenos@gmail.com>
2021-01-30 12:18:39 +0100

    Crud Services created to implement a generic for all the common behaviors of the different services. Closes #10

--- pet-clinic-data/src/main/java/jejobueno/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/jejobueno/springframework/sfgpetclinic/services/VetService.java
@@ -2,13 +2,6 @@ package jejobueno.springframework.sfgpetclinic.services;
 
 import jejobueno.springframework.sfgpetclinic.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
-
-    Vet findByID(Long id);
-
-    Vet save(Vet Vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>{
 }
