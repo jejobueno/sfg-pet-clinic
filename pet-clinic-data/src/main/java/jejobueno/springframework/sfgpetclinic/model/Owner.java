6bba547792e91a4801580c82414a33104fb4305b
Jesus Bueno <jessbuenos@gmail.com>
2021-03-21 12:03:04 +0100

    Owner enhanced. closes #28

--- pet-clinic-data/src/main/java/jejobueno/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/jejobueno/springframework/sfgpetclinic/model/Owner.java
@@ -1,8 +1,45 @@
 package jejobueno.springframework.sfgpetclinic.model;
-
-import java.util.List;
+import java.util.Set;
 
 public class Owner extends Person {
 
-    private List<Pet> pets;
+    private String address;
+
+    private String telephone;
+
+    private String City;
+
+    private Set<Pet> pets;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String city) {
+        City = city;
+    }
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
